f361d8f75b151c9eae9f0e029e96170abe9c74d9
Dávid Ferenc Szabó <szabodavidferenc@gmail.com>
2021-07-05 16:19:13 +0200

    [backend][RegAlloc] Implementing new register allocator, which is a more basis version of the Linear Register Allocator

--- backend/RegisterAllocator.cpp
+++ backend/RegisterAllocator.cpp
@@ -7,23 +7,30 @@
 #include "TargetRegister.hpp"
 #include <algorithm>
 #include <vector>
+#include <iostream>
 
 using VirtualReg = unsigned;
 using PhysicalReg = unsigned;
+using LiveRangeMap = std::map<VirtualReg, std::pair<unsigned, unsigned>>;
 
 void PreAllocateParameters(MachineFunction &Func, TargetMachine *TM,
-                           std::map<VirtualReg, PhysicalReg> &AllocatedRegisters) {
+                           std::map<VirtualReg, PhysicalReg> &AllocatedRegisters,
+                           LiveRangeMap &LiveRanges) {
   auto ArgRegs = TM->GetABI()->GetArgumentRegisters();
   unsigned CurrentParamReg = 0;
 
-  for (auto Param : Func.GetParameters()) {
+  for (auto [ParamID, ParamLLT] : Func.GetParameters()) {
     // FIXME: excess parameters should be stored on the stack
     assert(CurrentParamReg < ArgRegs.size() && "Run out of param regs");
+
+    // set the parameter live
+    LiveRanges[ParamID] = {0, ~0};
+
     // allocate the param to the CurrentParamReg -th param register
-    if (Param.second.GetBitWidth() <= 32)
-      AllocatedRegisters[Param.first] = ArgRegs[CurrentParamReg++]->GetSubRegs()[0];
+    if (ParamLLT.GetBitWidth() <= 32)
+      AllocatedRegisters[ParamID] = ArgRegs[CurrentParamReg++]->GetSubRegs()[0];
     else
-      AllocatedRegisters[Param.first] = ArgRegs[CurrentParamReg++]->GetID();
+      AllocatedRegisters[ParamID] = ArgRegs[CurrentParamReg++]->GetID();
   }
 }
 
@@ -76,42 +83,23 @@ PhysicalReg GetNextAvailableReg(uint8_t BitSize, std::vector<PhysicalReg> &Pool,
   return 0;
 }
 
-PhysicalReg GetMatchingSizedRegFromSubRegs(PhysicalReg PhysReg, uint8_t BitSize,
-                                           TargetMachine *TM) {
-  auto PhysRegInfo = TM->GetRegInfo()->GetRegisterByID(PhysReg);
-
-  // if the register size is not matching the actual operand size
-  // in bits then search for the subregisters as well for actual
-  // matching one
-  if (PhysRegInfo->GetBitWidth() == BitSize)
-    return PhysReg;
-
-  for (auto SubReg : PhysRegInfo->GetSubRegs()) {
-    auto PhysSubRegInfo = TM->GetRegInfo()->GetRegisterByID(SubReg);
-
-    if (PhysSubRegInfo->GetBitWidth() == BitSize)
-      return SubReg;
-  }
-
-  return ~0u;
-}
-
+// TODO: Add handling for spilling registers
 void RegisterAllocator::RunRA() {
-  // mapping from virtual reg to physical
-  std::map<VirtualReg, PhysicalReg> AllocatedRegisters;
-  std::vector<PhysicalReg> RegisterPool;  // available registers
-  std::set<MachineOperand> RegistersToSpill; // register require spilling
-
   for (auto &Func : MIRM->GetFunctions()) {
-    AllocatedRegisters.clear();
-    RegisterPool.clear();
-    RegistersToSpill.clear();
+    // mapping virtual registers to live ranges, where the live range represent
+    // the pair of the first definition (def) of the virtual register and the
+    // last use (kill) of it. Kill initialized to ~0 to signal errors
+    // potentially dead regs in the future
+    LiveRangeMap LiveRanges;
+    std::map<VirtualReg, MachineOperand*> VRegToMOMap;
+    std::map<VirtualReg, PhysicalReg> AllocatedRegisters;
+    std::vector<PhysicalReg> RegisterPool;
 
     // Initialize the usable register's pool
     for (auto TargetReg : TM->GetABI()->GetCallerSavedRegisters())
       RegisterPool.push_back(TargetReg->GetID());
 
-    PreAllocateParameters(Func, TM, AllocatedRegisters);
+    PreAllocateParameters(Func, TM, AllocatedRegisters, LiveRanges);
     PreAllocateReturnRegister(Func, TM, AllocatedRegisters);
 
     // Remove the pre allocated registers from the register pool
@@ -130,171 +118,147 @@ void RegisterAllocator::RunRA() {
       }
     }
 
-    // we want to keep track of how many consecutive renames happened since only
-    // two can be afforded for now, doing more then that is an error
-    unsigned ConsecutiveLoadRenames = 0;
-    unsigned ConsecutiveStoreRenames = 0;
-
+    // Calculating the live ranges for the virtual registers
+    unsigned InstrCounter = 0;
     for (auto &BB : Func.GetBasicBlocks())
-      for (auto &Instr : BB.GetInstructions())
-        for (auto &Operand : Instr.GetOperands())
-          if (Operand.IsVirtualReg() || Operand.IsParameter()) {
-            const auto UsedReg = Operand.GetReg();
-            bool AlreadyAllocated = 0 != AllocatedRegisters.count(UsedReg);
+      for (auto &Instr : BB.GetInstructions()) {
+        for (size_t i = 0; i < Instr.GetOperandsNumber(); i++) {
+          auto &Operand = Instr.GetOperands()[i];
 
-            // when short on allocatable registers, then add the register
-            // to the spillable set if its not a load or a store
-            if (!AlreadyAllocated && RegisterPool.size() <= 3) {
-              // if the instruction is NOT a load, then spill it
-              if (!Instr.IsLoadOrStore()) {
-                ConsecutiveLoadRenames = ConsecutiveStoreRenames = 0;
-                RegistersToSpill.insert(Operand);
-                continue;
-              }
-
-              // Depending on whether its a load or a store, rename the register
-              // (allocating essentially)
-              if (Instr.IsLoad()) {
-                // Note that the last 2 register is used for this purpose
-                assert(ConsecutiveLoadRenames < 2);
-                auto PhysReg =
-                    RegisterPool.rbegin()[1 - ConsecutiveLoadRenames];
-                auto FoundPhysReg = GetMatchingSizedRegFromSubRegs(
-                    PhysReg, Operand.GetSize(), TM);
-                assert(FoundPhysReg != ~0u &&
-                       "Cannot found matching sized register");
-                AllocatedRegisters[UsedReg] = FoundPhysReg;
-                ConsecutiveLoadRenames++;
-              } else {
-                auto PhysReg = RegisterPool.rbegin()[2];
-                auto FoundPhysReg = GetMatchingSizedRegFromSubRegs(
-                    PhysReg, Operand.GetSize(), TM);
-                assert(FoundPhysReg != ~0u &&
-                       "Cannot found matching sized register");
-                AllocatedRegisters[UsedReg] = FoundPhysReg;
-                ConsecutiveStoreRenames++;
-              }
-
-              // if its a load then rename it, which means to allocate it one of
-              // the registers used for loading spilled regs
-              assert(ConsecutiveLoadRenames <= 2 &&
-                     "To much consecutive loads to rename");
-              assert(ConsecutiveStoreRenames <= 1 &&
-                     "To much consecutive stores to rename");
-
-              AlreadyAllocated = true;
-            }
-
-            // if not allocated yet
-            if (!AlreadyAllocated) {
-              // then allocate it
-              auto Reg =
-                  GetNextAvailableReg(Operand.GetSize(), RegisterPool, TM);
-              AllocatedRegisters[UsedReg] = Reg;
-              Operand.SetToRegister();
-              Operand.SetReg(Reg);
-            }
-            // else its already allocated so just look it up
-            else {
-              Operand.SetToRegister();
-              Operand.SetReg(AllocatedRegisters[UsedReg]);
-            }
-          }
-
-    ///////////// Handle Spills
-
-    // if there is nothing to spill then nothing to do
-    if (RegistersToSpill.size() != 0) {
-      for (auto &Reg : RegistersToSpill)
-        Func.InsertStackSlot(Reg.GetReg(), Reg.GetSize() / 8);
-
-      for (auto &BB : Func.GetBasicBlocks()) {
-        auto &Instructions = BB.GetInstructions();
-
-        for (size_t i = 0; i < Instructions.size(); i++) {
-
-          // Check operands which use the register
-          // (the first operand defining the reg the rest uses regs)
-          for (size_t OpIndex = 1;
-               OpIndex < Instructions[i].GetOperands().size(); OpIndex++) {
-            auto &Operand = Instructions[i].GetOperands()[OpIndex];
-
-            // only check register operands
-            if (!(Operand.IsRegister() || Operand.IsMemory()))
-              continue;
-
+          if (Operand.IsVirtualReg() || Operand.IsParameter() ||
+              Operand.IsMemory()) {
             auto UsedReg = Operand.GetReg();
+            // Save the VReg Operand into a map to be able to look it up later
+            // for size information like its bit size
+            if (VRegToMOMap.count(UsedReg) == 0)
+              VRegToMOMap[UsedReg] = &Instr.GetOperands()[i];
 
-            // if the used register is not spilled, then nothing to do
-            if (RegistersToSpill.count(Operand) == 0)
-              continue;
+            // if this VirtualReg first encountered
+            // for now assuming also its a definition if we encountered it first
+            if (LiveRanges.count(UsedReg) == 0)
+              LiveRanges[UsedReg] = {InstrCounter, ~0};
 
-            ////// Insert a LOAD
-
-            auto FoundReg =
-                GetMatchingSizedRegFromSubRegs(RegisterPool[OpIndex],
-                                               Operand.GetSize(), TM);
-            assert(FoundReg != ~0u && "Cannot found matching sized register");
-
-            Operand.SetReg(FoundReg);
-
-            // Saving the original Operand to be able to use its information
-            // when creating the LOAD
-            auto OperandSave = Operand;
-
-            // Make the operand into a physical register which using
-            // the register used for the spilling
-            if (!Operand.IsMemory())
-              Operand.SetToRegister();
+            // otherwise it was already seen (therefore defined) so we only
+            // have to update the LiveRange entry last use part
             else
-              Operand.SetVirtual(false);
+              LiveRanges[UsedReg].second = InstrCounter;
 
-            auto Load = MachineInstruction(MachineInstruction::LOAD, &BB);
-            /// NOTE: 3 register left in pool using the 2nd and 3rd for uses
-            OperandSave.SetToRegister();
-            Load.AddOperand(OperandSave);
-            Load.AddStackAccess(UsedReg);
-            TM->SelectInstruction(&Load);
-
-            BB.InsertInstr(std::move(Load), i++);
           }
+        } // Operand end
+        InstrCounter++;
+      } // Instr end
 
-          /// Insert STORE if needed
-          auto &Operand = Instructions[i].GetOperands()[0];
-          auto DefReg = Operand.GetReg();
+#ifdef DEBUG
+    for (const auto &[VReg, LiveRange] : LiveRanges) {
+      auto [DefLine, KillLine] = LiveRange;
+      std::cout << "VReg: " << VReg << ", LiveRange(" << DefLine << ", "
+                << KillLine << ")" << std::endl;
+    }
+#endif
 
-          if (RegistersToSpill.count(Operand) == 0)
-            continue;
+    // make a sorted vector from the map where the element ordered by the
+    // LiveRange kill field, if both kill field is equal then the def field will
+    // decide it
+    std::vector<std::tuple<unsigned, unsigned, unsigned>> SortedLiveRanges;
+    for (const auto &[VReg, LiveRange] : LiveRanges) {
+      auto [DefLine, KillLine] = LiveRange;
+      SortedLiveRanges.push_back({VReg, DefLine, KillLine});
+    }
+    std::sort (SortedLiveRanges.begin(), SortedLiveRanges.end(),
+              [](std::tuple<unsigned, unsigned, unsigned> Left,
+                 std::tuple<unsigned, unsigned, unsigned> Right) {
+                auto [LVReg, LDef, LKill] = Left;
+                auto [RVReg, RDef, RKill] = Right;
 
-          auto FoundReg =
-              GetMatchingSizedRegFromSubRegs(RegisterPool[0],
-                                             Operand.GetSize(), TM);
-          assert(FoundReg != ~0u && "Cannot found matching sized register");
+                if (LKill < RKill)
+                  return true;
+                else if (LKill == RKill)
+                  return LDef < RDef;
+                else
+                  return false;
+    });
 
-          Operand.SetReg(FoundReg);
+#ifdef DEBUG
+    std::cout << "SortedLiveRanges" << std::endl;
+    for (const auto &[VReg, DefLine, KillLine] : SortedLiveRanges)
+      std::cout << "VReg: " << VReg << ", LiveRange(" << DefLine << ", "
+                << KillLine << ")" << std::endl;
+    std::cout << std::endl;
+#endif
 
-          auto OperandSave = Operand;
+    // Where to start the search from registers to be freed. Entries with
+    // smaller indexes were already freed, therefore don't have to check them.
+    unsigned StartIndex = 0;
+    unsigned CurrentIdx = 0;
+    for (const auto &[VReg, DefLine, KillLine] : SortedLiveRanges) {
+      // if not allocated yet, then allocate it
+      if (AllocatedRegisters.count(VReg) == 0)
+        AllocatedRegisters[VReg] =
+            GetNextAvailableReg(VRegToMOMap[VReg]->GetSize(), RegisterPool, TM);
 
-          if (!Operand.IsMemory())
-            Operand.SetToRegister();
-          else
-            Operand.SetVirtual(false);
+      // free registers which are already killed
+      unsigned CheckCurrentIdx = 0;
+      for (const auto [CheckVReg, CheckDefLine, CheckKillLine] :
+           SortedLiveRanges) {
+        // only check for the current and the previous entries
+        if (CheckCurrentIdx >= CurrentIdx)
+          break;
 
-          auto Store = MachineInstruction(MachineInstruction::STORE, &BB);
-          Store.AddStackAccess(DefReg);
-          /// NOTE: 3 register left in pool using the 1st for def
-          OperandSave.SetToRegister();
-          Store.AddOperand(OperandSave);
-          TM->SelectInstruction(&Store);
+        // only start doing something when the start index of the non freed
+        // entries reached first and the above checked entry definitions line
+        // is greater then this entry kill line. Meaning the register assigned
+        // to this entry can be freed, since we already passed the line where it
+        // was last used (killed)
+        if (CheckCurrentIdx >= StartIndex && CheckKillLine < DefLine) {
+          // Freeing the register allocated to this live range's register
+          // If its a subregister then we have to find its parent first and then
+          // put that back to the allocatable register's RegisterPool
+          assert(AllocatedRegisters.count(CheckVReg) > 0);
+          unsigned FreeAbleReg = AllocatedRegisters[CheckVReg];
+          auto ParentReg = TM->GetRegInfo()->GetParentReg(FreeAbleReg);
+          if (ParentReg)
+            FreeAbleReg = ParentReg->GetID();
 
-          BB.InsertInstr(std::move(Store), ++i);
+#ifdef DEBUG
+          std::cout << "Freed register "
+                    << TM->GetRegInfo()->GetRegisterByID(FreeAbleReg)->GetName()
+                    << std::endl;
+#endif
+          RegisterPool.insert(RegisterPool.begin(), FreeAbleReg);
+          StartIndex++;
         }
+        CheckCurrentIdx++;
       }
+      CurrentIdx++;
     }
 
-  // FIXME: Move this out from here and make it a PostRA pass
-  // After RA lower the stack accessing operands to their final form
-  // based on the final stack frame
+#ifdef DEBUG
+    std::cout << std::endl << std :: endl << "AllocatedRegisters" << std::endl;
+    for (auto [VReg, PhysReg] : AllocatedRegisters)
+      std::cout << "VReg: " << VReg << " to " <
+          < TM->GetRegInfo()->GetRegisterByID(PhysReg)->GetName() << std::endl;
+    std::cout << std::endl << std::endl;
+#endif
+
+    // Setting to operands from virtual register to register as a last part of
+    // the allocation
+    for (auto &BB : Func.GetBasicBlocks())
+      for (auto &Instr : BB.GetInstructions())
+        for (size_t i = 0; i < Instr.GetOperandsNumber(); i++) {
+          auto &Operand = Instr.GetOperands()[i];
+          auto PhysReg = AllocatedRegisters[Operand.GetReg()];
+          if (Operand.IsVirtualReg() || Operand.IsParameter()) {
+            Operand.SetToRegister();
+            Operand.SetReg(PhysReg);
+          } else if (Operand.IsMemory()) {
+            Operand.SetVirtual(false);
+            Operand.SetValue(PhysReg);
+          }
+        }
+
+    // FIXME: Move this out from here and make it a PostRA pass
+    // After RA lower the stack accessing operands to their final form
+    // based on the final stack frame
     for (auto &BB : Func.GetBasicBlocks())
       for (auto &Instr : BB.GetInstructions()) {
         // Check the operands
@@ -333,5 +297,5 @@ void RegisterAllocator::RunRA() {
           break; // there should be only at most one stack access / instr
         }
       }
-  }
+  } // Func end
 }
