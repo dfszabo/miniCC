75d45bd9e21c3f20b828be36e5204c57e966b6dd
Dávid Ferenc Szabó <szabodavidferenc@gmail.com>
2021-07-17 16:29:03 +0200

    [Backend][Fix] Fixing the issue of not returning unique virtual registers with GetNextAvailableVReg function

--- backend/MachineFunction.cpp
+++ backend/MachineFunction.cpp
@@ -8,14 +8,17 @@ unsigned MachineFunction::GetNextAvailableVReg() {
   // If this function was called the first time then here the highest virtual
   // register ID is searched and NextVReg is set to that.
   for (auto &[ParamID, ParamLLT] : Parameters)
-    if (ParamID > NextVReg)
+    if (ParamID == NextVReg)
+      NextVReg++;
+    else if (ParamID > NextVReg)
       NextVReg = ParamID;
 
   for (auto &BB : BasicBlocks)
     for (auto &Instr : BB.GetInstructions())
       for (auto &Operand : Instr.GetOperands())
-        if (Operand.IsVirtualReg() && Operand.GetReg() > NextVReg)
-          NextVReg = Operand.GetReg();
+        if (Operand.IsVirtualReg() && Operand.GetReg() >= NextVReg)
+          NextVReg =
+              Operand.GetReg() == NextVReg ? NextVReg + 1 : Operand.GetReg();
 
   // The next one is 1 more then the found highest
   return NextVReg++;
