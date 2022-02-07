1c3feea78349f902bb2ed2eb07f55869de80c6b1
Dávid Ferenc Szabó <szabodavidferenc@gmail.com>
2022-02-07 21:39:53 +0100

    [Test] Adding support to run specific test files. Adding -save-temps command line flag to save the temporary files, which can be used for debug purposes.

--- tests/TestRun.py
+++ tests/TestRun.py
@@ -2,7 +2,9 @@ import os
 import re
 import subprocess
 from os import path
+import sys
 
+save_temps = False
 
 def check_file(file_name):
     arch = ""
@@ -18,7 +20,7 @@ def check_file(file_name):
             if m:
                 function_declarations.append(m.group(1))
 
-            m = re.search(r'(?:/{2}|#) *TEST-CASE: (.*) -> (\d+)', line)
+            m = re.search(r'(?:/{2}|#) *TEST-CASE: (.*) -> (-?\d+)', line)
             if m:
                 test_cases.append((m.group(1), m.group(2)))
 
@@ -46,7 +48,7 @@ def execute_tests(file_name, arch, function_declarations, test_cases):
     test_asm = subprocess.check_output(["../build/miniCC", file_name]).decode("utf-8")
 
     text_file = open("test.s", "w")
-    n = text_file.write(test_asm)
+    text_file.write(test_asm)
     text_file.close()
 
     for case, expected_result in test_cases:
@@ -55,63 +57,88 @@ def execute_tests(file_name, arch, function_declarations, test_cases):
         current_test_main = current_test_main.replace("@", expected_result)
 
         text_file = open("test_main.c", "w")
-        n = text_file.write(current_test_main)
+        text_file.write(current_test_main)
         text_file.close()
 
         compile_ret = subprocess.run([arch + "-linux-gnu-gcc", "test_main.c", "test.s", "-o", "test", "-static"]).returncode
         if compile_ret != 0:
-            if path.exists("test_main.c"):
-                os.remove("test_main.c")
-            if path.exists("test.s"):
-                os.remove("test.s")
-            if path.exists("test"):
-                os.remove("test")
+            if not save_temps:
+              if path.exists("test_main.c"):
+                  os.remove("test_main.c")
+              if path.exists("test.s"):
+                  os.remove("test.s")
+              if path.exists("test"):
+                  os.remove("test")
             return False
 
         ret_code = subprocess.run([run_command, "test"]).returncode
 
         if ret_code != 0:
-            os.remove("test_main.c")
-            os.remove("test.s")
-            os.remove("test")
+            if not save_temps:
+              os.remove("test_main.c")
+              os.remove("test.s")
+              os.remove("test")
             return False
 
-    if path.exists("test_main.c"):
-        os.remove("test_main.c")
-    if path.exists("test.s"):
-        os.remove("test.s")
-    if path.exists("test"):
-        os.remove("test")
+    if not save_temps:
+      if path.exists("test_main.c"):
+          os.remove("test_main.c")
+      if path.exists("test.s"):
+          os.remove("test.s")
+      if path.exists("test"):
+          os.remove("test")
     return True
 
+# clean up files remaining from previous runs
+if path.exists("test_main.c"):
+  os.remove("test_main.c")
+if path.exists("test.s"):
+  os.remove("test.s")
+if path.exists("test"):
+  os.remove("test")
 
+test_set = []
 failed_tests = []
 tests_count = 0
 passed_tests_count = 0
-for subdir, dirs, files in os.walk(os.getcwd()):
-    for filename in files:
-        filepath = subdir + os.sep + filename
 
-        if filepath.endswith(".c") or filepath.endswith(".s"):
-            arch, function_declarations, test_cases = check_file(filepath)
-            if arch == "":
-                continue
+# handle program arguments
+for i, arg in enumerate(sys.argv):
+  if arg == "-save-temps":
+    save_temps = True
+  else:
+    # treat non flag arguments as test files
+    test_set.append(arg)
 
-            print(filepath, end="")
+# if no input files were given, then save all test file from the current directory
+if len(test_set) == 0:
+  for subdir, dirs, files in os.walk(os.getcwd()):
+      for filename in files:
+          filepath = subdir + os.sep + filename
 
-            success = execute_tests(filepath, arch, function_declarations, test_cases)
+          if filepath.endswith(".c") or filepath.endswith(".s"):
+              test_set.append(filepath)
 
-            tests_count += 1
-            if success:
-                passed_tests_count += 1
-                print("...PASS")
-            else:
-                failed_tests.append(filepath)
-                print("...FAIL")
+# run the tests
+for filepath in test_set:
+  arch, function_declarations, test_cases = check_file(filepath)
+  if arch == "":
+      continue
 
+  success = execute_tests(filepath, arch, function_declarations, test_cases)
+
+  tests_count += 1
+  if success:
+      passed_tests_count += 1
+      print("PASS :: ", filepath)
+  else:
+      failed_tests.append(filepath)
+      print("FAIL :: ", filepath)
+
+# print test results
 print("\n--------", tests_count, "Test executed --------")
-print("\t\t", passed_tests_count, "PASS")
-print("\t\t", len(failed_tests), "FAIL\n")
+print("\t\t", passed_tests_count, " PASS")
+print("\t\t", len(failed_tests), " FAIL\n")
 
 for test_case in failed_tests:
     print(test_case)
