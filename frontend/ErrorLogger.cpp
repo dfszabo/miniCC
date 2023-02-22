42ab9d91801cde08b3a2bce0e9dbb70afcdbdda5
Dávid Ferenc Szabó <szabodavidferenc@gmail.com>
2023-02-22 21:15:05 +0100

    [NFC][Frontend] Minor ErrorLogger clean up

--- frontend/ErrorLogger.cpp
+++ frontend/ErrorLogger.cpp
@@ -12,45 +12,46 @@ void ErrorLogger::AddMessage(const std::string &Msg) {
 }
 
 void ErrorLogger::AddMessage(const std::string &Msg, const char *Type) {
-  ErrorMessages.push_back(Type + Msg);
+  ErrorMessages.push_back(std::string(": ") + Type + std::string(": ") + Msg);
 }
 
 void ErrorLogger::AddMessage(const std::string &Msg, const char *Type,
                              const Token &T) {
   assert(T.GetLineNum() < Source.size() && "Out of bound index");
   std::string MsgWithLineNums = ":" + std::to_string(T.GetLineNum() + 1) + ":" +
-                                std::to_string(T.GetColNum() + 1) + Type + Msg +
-                                "\n" + Source[T.GetLineNum()] + "\n" +
+                                std::to_string(T.GetColNum() + 1) +
+                                std::string(": ") + Type + std::string(": ") +
+                                Msg + "\n" + Source[T.GetLineNum()] + "\n" +
                                 CreateCodePointerString(T);
   AddMessage(MsgWithLineNums);
 }
 
 void ErrorLogger::AddError(const std::string &Msg) {
   HasError = true;
-  AddMessage(Msg, ": error: ");
+  AddMessage(Msg, "error");
 }
 
 void ErrorLogger::AddError(const std::string &Msg, const Token &T) {
   HasError = true;
-  AddMessage(Msg, ": error: ", T);
+  AddMessage(Msg, "error", T);
 }
 
 void ErrorLogger::AddWarning(const std::string &Msg) {
   HasWarning = true;
-  AddMessage(Msg, ": warning: ");
+  AddMessage(Msg, "warning");
 }
 
 void ErrorLogger::AddWarning(const std::string &Msg, const Token &T) {
   HasWarning = true;
-  AddMessage(Msg, ": warning: ", T);
+  AddMessage(Msg, "warning", T);
 }
 
 void ErrorLogger::AddNote(const std::string &Msg) {
-  AddMessage(Msg, ": note: ");
+  AddMessage(Msg, "note");
 }
 
 void ErrorLogger::AddNote(const std::string &Msg, const Token &T) {
-  AddMessage(Msg, ": note: ", T);
+  AddMessage(Msg, "note", T);
 }
 
 bool ErrorLogger::HasErrors(const bool Wall) const {
